c8fb80c5f6f5863474b14e62f31330cc8cff3ad0
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2023-11-11 12:22:33 +0300

    add new display futures

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
+from django.views.generic.base import RedirectView
+
+favicon_view=RedirectView.as_view(url="/templates",permanent=True)
+
 urlpatterns = [
     path('',views.index,name="index"),
+    re_path(r'^favicon\.ico$',favicon_view),
     path("about/",views.about,name="about"),
     path("create/",views.create,name="create"),
     path("toggle_finished/<todo_id>",views.toggle_finished,name="toggle_finished"),
